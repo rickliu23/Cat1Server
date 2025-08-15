5162edef311e8978c7044f5dd024f2f7b38d7bd7
RickLiu <Rick@linposh.com.cn>
2025-08-15 16:14:56 +0800

    增加设备回复命令表

--- CatLib/Cat1/srvLte_Cat1Cmd.h
+++ CatLib/Cat1/srvLte_Cat1Cmd.h
@@ -6,13 +6,32 @@ extern "C" {
 #endif
 
 #include "stdbool.h"
+#include "stdlib.h"
 #include "srvLte_Def.h"
 
 #ifdef DEFINE_LTE_AT_COMMANDS
-    #define DECLARE_AT_COMMAND(name, value) const char name[] = value
+#define DECLARE_AT_COMMAND(name, value) const char name[] = value
 #else
-    #define DECLARE_AT_COMMAND(name, value) extern const char name[]
+#define DECLARE_AT_COMMAND(name, value) extern const char name[]
 #endif
+
+typedef enum
+{
+    LTE_AT_CMD_UNKNOW,
+    LTE_AT_CMD_AT, //test AT communication
+    LTE_AT_CMD_ECHO, // echo on/off
+    LTE_AT_CMD_ATI, // get module info
+    LTE_AT_CMD_CSQ, // get signal quality
+    LTE_AT_CMD_FUNC, // flight mode on/off
+    LTE_AT_CMD_SIM_PIN, // check SIM card status
+    LTE_AT_CMD_NET_REG, // network registration status
+
+
+    LTE_AT_CMD_CALL,
+    LTE_AT_CMD_UNCALL,
+
+    LTE_AT_CMD_NUM
+} LTE_AT_CMD_TYPE;
 
 typedef enum
 {
@@ -22,7 +41,7 @@ typedef enum
     LTE_AT_NUM
 } LTE_AT_OPERATION_TYPE;
 
-/* CMD */
+/************************* CMD *************************/
 DECLARE_AT_COMMAND(LTE_AT_AT, "AT");
 DECLARE_AT_COMMAND(LTE_AT_ECHO, "E");
 DECLARE_AT_COMMAND(LTE_AT_SIM_PIN, "CPIN");
@@ -39,54 +58,41 @@ DECLARE_AT_COMMAND(LTE_AT_ERR_RSP, "ERROR");
 DECLARE_AT_COMMAND(LTE_AT_READY_RSP, "READY");
 
 
-typedef enum
+
+
+
+// When defining AT commands, explicitly associate them with response types.?
+typedef struct
 {
-    LTE_AT_CMD_UNKNOW,
-    LTE_AT_CMD_AT, //test AT communication
-    LTE_AT_CMD_ECHO, // echo on/off
-    LTE_AT_CMD_ATI, // get module info
-    LTE_AT_CMD_CSQ, // get signal quality
-    LTE_AT_CMD_FUNC, // flight mode on/off
-    LTE_AT_CMD_SIM_PIN, // check SIM card status
-    LTE_AT_CMD_NET_REG, // network registration status
-
-    
-    LTE_AT_CMD_CALL,
-    LTE_AT_CMD_UNCALL,
+    LTE_AT_CMD_TYPE cmd_id;
+    const char *cmd_str;
+    const char *ok_rsp; // succeed response
+    const char *err_rsp; // error response
+    const char *data_rsp; // response with data
+} Lte_AT_Response_Table_TYPE;
 
 
 
-    LTE_AT_CMD_NUM
-} LTE_AT_CMD_TYPE;
+#ifdef DEFINE_LTE_AT_COMMANDS
+#define DECLARE_CMD_REGISTRY(id, str, ok, err, data) \
+    {id, str, ok, err, data}
 
+const Lte_AT_Response_Table_TYPE Lte_AT_Response_Table[] = 
+{
+    DECLARE_CMD_REGISTRY(LTE_AT_CMD_AT, LTE_AT_AT, LTE_AT_OK_RSP, NULL, NULL),
+    DECLARE_CMD_REGISTRY(LTE_AT_CMD_ECHO, LTE_AT_ECHO, LTE_AT_OK_RSP, LTE_AT_ERR_RSP, NULL),
+    DECLARE_CMD_REGISTRY(LTE_AT_CMD_SIM_PIN, LTE_AT_SIM_PIN, LTE_AT_OK_RSP, LTE_AT_ERR_RSP, LTE_AT_SIM_PIN),
+    DECLARE_CMD_REGISTRY(LTE_AT_CMD_CSQ, LTE_AT_CSQ, NULL, NULL, LTE_AT_CSQ),
+    DECLARE_CMD_REGISTRY(LTE_AT_CMD_NET_REG, LTE_AT_NET_REG, LTE_AT_OK_RSP, LTE_AT_ERR_RSP, LTE_AT_NET_REG)
+};
 
+const int Lte_AT_Response_Table_Size = sizeof(Lte_AT_Response_Table)/sizeof(Lte_AT_Response_Table_TYPE);
+#else
+    extern const Lte_AT_Response_Table_TYPE Lte_AT_Response_Table[];
+    extern const int Lte_AT_Response_Table_Size;
+#endif
 
 bool Lte_AT_Assemble_Basic(LTE_AT_CMD_TYPE id, LTE_AT_OPERATION_TYPE type, int param, char cmdBuf[], int bufMaxLen, int *usefulLen);
-//// 公共结构体定义
-//typedef struct  {
-//    _LTE_AT_INDEX at_index;
-//    double timeout;
-//    const char* server_name;
-//} GlobalConfig;
-
-//// 根据宏定义判断声明或定义
-//#ifdef DEFINE_GLOBALS
-//    #define EXTERN // 空定义
-//    #define INIT(...) = __VA_ARGS__
-//#else
-//    #define EXTERN extern
-//    #define INIT(...) // 空初始化
-//#endif
-
-//// 全局配置实例（宏控制定义或声明）
-//EXTERN GlobalConfig g_config INIT({
-//    .at_index = LTE_AT_CMD_AT, 
-//    .timeout = 5.0,
-//    .server_name = "default"
-//});
-
-//#undef EXTERN
-//#undef INIT
 
 #ifdef __cplusplus
 }
